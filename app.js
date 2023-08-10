a973b52e4ebaed57080bad2b1d57bdc63e2d1b1f
Shreyas <sonureigns007@gmail.com>
2023-08-10 10:38:20 +0530

    colored bg added

--- app.js
+++ app.js
@@ -8,7 +8,20 @@
 //   document.documentElement.style.setProperty("--accent-color", randomColor);
 // });
 
-const changeColor = e => {
+const changeColor = (e, x) => {
+    let homeBg = document.getElementById("Home");
+    if (x === 1) {
+      homeBg.style.backgroundImage = 'url("./imgs/img2.jpg")';
+    } else if (x === 2) {
+      homeBg.style.backgroundImage = 'url("./chidiyaud.jpeg")';
+    } else if (x === 3) {
+      homeBg.style.backgroundImage = 'url("./imgs/img3.jpg")';
+    } else if (x === 4) {
+      homeBg.style.backgroundImage =
+        'url("./HT_vulnerabilities_GettyImages-923729070.webp")';
+    } else if (x === 5) {
+      homeBg.style.backgroundImage = 'url("./imgs/img4.jpg")';
+    }
     const colorPalette=`#${e}`
     document.documentElement.style.setProperty("--accent-4", colorPalette);
 };
